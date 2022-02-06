b5a32a1eb1a71701b767efb93cc8d626404be92d
Subhadipa <bsubhadipa@gmail.com>
2022-02-06 18:57:33 +0530

    Project Done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController = require("../controllers/authorController")
-const blogController = require("../controllers/blogController")
-const middleWare = require("../middleware/authentication")
-const Token = middleWare.tokenCheck
 
-router.post('/authors',  authorController.createAuthor);
+const urlController = require("../controllers/urlController")
 
-router.post('/login', authorController.loginAuthor)
-
-router.post('/blogs', Token, blogController.createBlog);
-
-router.get('/blogs', Token, blogController.getFilterBlog);
-
-router.put('/blogs/:blogId', Token, blogController.updateBlog);
-
-router.delete('/blogs/:blogId', Token, blogController.deleteBlogbyId);
-
-router.delete('/blogs', Token, blogController.deleteBlog)
-
+router.post('/url/shorten', urlController.createUrl);
+router.get('/:urlCode', urlController.getUrl);
 
 
 module.exports = router;
